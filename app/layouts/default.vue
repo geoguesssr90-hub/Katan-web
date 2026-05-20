6a6ac232d90a3e82be0d0dd7a037dfe78548d9a0
geoguesssr90-hub <geoguesssr90@gmail.com>
2026-05-20 11:08:36 +0900

    add a useHead in default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -16,4 +16,21 @@
 
 <script setup lang="ts">
 import '@/assets/css/pageTop.css'
+
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@type': 'WebSite',
+        name: 'カタンについて（非公式団体）',
+        url: 'https://catan-web-one.vercel.app/',
+        inLanguage: 'ja',
+        description: 'カタンのルール、カタンルール詳細'
+      })
+    }
+  ]
+})
+
 </script>
